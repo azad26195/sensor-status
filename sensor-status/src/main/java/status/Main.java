6e194cc75469200d4cca829a91d9ae710e911e2b
Azad Vishwakarma <azad26195@gmail.com>
2016-02-19 20:11:26 +0530

    IMPROVED - GroupBy Function added for sorting the sensor id and timestamp.

--- sensor-status/src/main/java/status/Main.java
+++ sensor-status/src/main/java/status/Main.java
@@ -1,4 +1,5 @@
 package status;
+
 import cascading.flow.Flow;
 import cascading.flow.FlowDef;
 import cascading.flow.FlowProcess;
@@ -19,6 +20,8 @@ import cascading.tap.partition.DelimitedPartition;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import cascading.tuple.TupleEntry;
+import cascading.pipe.GroupBy;
+
 
 import java.util.Calendar;
 import java.util.Properties;
@@ -35,27 +38,31 @@ public class Main{
 
 	    // Input file
 		String inputPath = args[ 0 ];
-
 	    // Output file
 		String outputPath = args[ 1 ];
 
 		Hfs inTap = new Hfs( new TextLine(new Fields("offset", "line")), inputPath );
-
 	    // Create a sink tap to write to the Hfs; by default, TextDelimited writes all fields out
 		Hfs outTap = new Hfs( new TextDelimited( true, "," ), outputPath, SinkMode.REPLACE );
 		
 		// Create a tuple of the input data
-		Fields tupleField = new Fields("version", "sensorid", "serverts");
+		Fields tupleField = new Fields("version", "sensorID", "serverTS");
 		
 		//Splits the input data using seperator ,
 		Function splitter = new Splitter(tupleField);
-		
 		Pipe processPipe = new Each("processPipe", new Fields("line"), splitter, tupleField);
+	
+		//Create a groupBy pipe for sorting the data according to the sensorid and timestamp
+		Pipe groupByPipe = new Pipe("sc", processPipe);
+		groupByPipe = new GroupBy(groupByPipe, new Fields("sensorID"), new Fields("serverTS"));
+		
+		
+
 
 			
 		FlowDef flowDef = FlowDef.flowDef()
 							.addSource(processPipe, inTap)
-							.addTailSink(processPipe,outTap);
+							.addTailSink(groupByPipe,outTap);
 		Flow wcFlow = flowConnector.connect(flowDef);
 		wcFlow.complete();
 
