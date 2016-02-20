377526f6d7519ee75bee650c1e1b7d0d6696f52d
azad <azad26195@gmail.com>
2016-02-20 11:22:07 +0530

    Delete start-hadoop.sh

--- start-hadoop.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-export HADOOP_HOME=/usr/local/hadoop 
-echo "---------------------Starting HADOOP DFS--------------------------"
-. $HADOOP_HOME/sbin/start-dfs.sh
-
-echo "---------------------Starting YARN-----------------------------------"
-. $HADOOP_HOME/sbin/start-yarn.sh
-
